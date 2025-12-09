16de7f6aacab80c023d9f9f5276c7094114dc635
ek101820 <kuburelif9@gmail.com>
2025-12-09 13:10:35 +0300

    Create switch-case-ornekler.py

---
+++ switch-case-ornekler.py
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+ /*int sayi;
+ printf("sayi giriniz:");
+ scanf("%d",&sayi);
+ switch(sayi) {
+ case 1:{ printf("bır"); break;}
+ case 2:{ printf("iki"); break;}
+ case 3:{ printf("uc"); break;}
+ case 4:{ printf("dort"); break;}
+ default : {printf("gecersiz sayi");break;}
+}*/
+
+/*int sifre;
+ printf("sifreyi giriniz:");
+ scanf("%d",&sifre);
+ switch(sifre) {
+case 101820:{
+int a,b;
+ printf("sayi1 giriniz:");
+ scanf("%d",&a);
+  printf("sayi2 giriniz:");
+ scanf("%d",&b);
+ printf("toplam %d",a+b);
+ break;}
+default: {printf("sifre yanlıs")1;break;}
+}
+*/
+/*
+int sifre;
+ printf("sifreyi giriniz:");
+ scanf("%d",&sifre);
+switch(sifre)  {
+case 111:{printf("elif\n"); break;}
+case 222:{printf("gaye\n"); break;}
+case 333:{printf("naz\n"); break;}
+default:{printf("gecersiz sayi\n");break;}
+
+}*/
+
+//sonsuz for döngüsü for(;;)
+/*int sifre;
+for(;;){
+ printf("sifreyi giriniz:");
+ scanf("%d",&sifre);
+switch(sifre)  {
+case 111:{printf("elif\n"); break;}
+case 222:{printf("gaye\n"); break;}
+case 333:{printf("naz\n"); break;}
+default:{printf("gecersiz sayi\n");break;}
+
+}
+
+}*/
+int gun;
+printf("bir sayi girin[1-7]:");
+scanf("%d",&gun);
+switch(gun) {
+case 1:printf("pazartesi");break;
+    case 2:printf("sali");break;
+        case 3:printf("carsamba");break;
+            case 4:printf("persembe");break;
+                case 5:printf("cuma");break;
+                    case 6:printf("cumartesi");break;
+                        case 7:printf("pazar");break;
+
+
+default : printf("gecersiz gun:");}
+}
+
+
